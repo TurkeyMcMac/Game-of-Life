605039eed88803d539cf15cd457c8aac1f34d022
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-08-04 19:01:19 -0400

    Make separate timer thread

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ use game_of_life::GameBoard;
 
 use std::env;
 use std::fs::File;
+use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
+
+const FRAME_DELAY: u64 = 70;
 
 fn main() {
     let mut file = File::open(&env::args().skip(1).next()
@@ -13,13 +16,24 @@ fn main() {
 
     let mut board = GameBoard::from_file(&mut file).unwrap();
 
-    loop {
+    let (tx, rx) = mpsc::sync_channel(1000);
+
+    thread::spawn(move || {
+        loop {
+            if let Err(_) = tx.send(NextStep) { return; }
+
+            thread::sleep(Duration::from_millis(FRAME_DELAY));
+        }
+    });
+
+    for _ in rx {
         board.ready();
 
         print!("{}[2J{}", 27 as char, board);
         
         board.step();
         
-        thread::sleep(Duration::from_millis(70));
     }
 }
+
+struct NextStep;
